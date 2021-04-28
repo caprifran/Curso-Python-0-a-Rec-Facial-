c42a46bf8b9b61f21d12d210c07d424d8f95df51
caprifran <81712123+caprifran@users.noreply.github.com>
2021-04-28 17:26:40 -0300

    Seccion 8

---
+++ Seccion 8/monedascontorno/contadormonedas.py
@@ -0,0 +1,24 @@
+from cv2 import cv2
+import numpy as np
+imgPath = "C:/Users/franc/Downloads/Curso Python - De 0 hasta reconocimiento facial/Seccion 8/monedascontorno/monedassoles.jpg"
+valorGauss = 1
+valorKernel = 33
+original = cv2.imread(imgPath)
+gris = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+gauss = cv2.GaussianBlur(gris, (valorGauss, valorGauss), 0)
+canny = cv2.Canny(gauss, 60, 100)
+kernel = np.ones((valorKernel, valorKernel), np.uint8)
+cierre = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
+
+contornos, jerarquias = cv2.findContours(
+    cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+print("Monedas encontradas: {}".format(len(contornos)))
+cv2.drawContours(original, contornos, -1, (0, 0, 255), 2)
+
+# Mostrar resultados
+# cv2.imshow("Grises", gris)
+# cv2.imshow("Gauss", gauss)
+# cv2.imshow("Canny", canny)
+cv2.imshow("Resultado", original)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
