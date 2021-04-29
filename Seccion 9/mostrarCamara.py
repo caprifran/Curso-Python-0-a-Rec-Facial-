314373614ae3c6da1969db7829bf9a21241fc109
caprifran <81712123+caprifran@users.noreply.github.com>
2021-04-28 21:10:30 -0300

    Seccion 9

---
+++ Seccion 9/mostrarCamara.py
@@ -0,0 +1,15 @@
+import cv2 as cv
+
+capturaVideo = cv.VideoCapture(1)
+if not capturaVideo.isOpened():
+    print("No se encontro una camara")
+    exit()
+while True:
+    tipoCam, camara = capturaVideo.read()
+    grises = cv.cvtColor(camara, cv.COLOR_BGR2GRAY)
+    
+    cv.imshow("En vivo", camara)
+    if cv.waitKey(1) == ord("q"):
+        break
+capturaVideo.release()
+cv.destroyAllWindows()
